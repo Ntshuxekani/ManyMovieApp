1f992f07ea84ec03b3741dd3713a761c9f0d772f
LesegoLekwadi <lesegolekwadi8@gmail.com>
2024-04-24 16:27:08 +0200

    reviw

--- MovieApp/src/app/app-routing.module.ts
+++ MovieApp/src/app/app-routing.module.ts
@@ -1,10 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { NavbarComponent } from './navbar/navbar.component';
+import { NavbarComponent,} from './navbar/navbar.component';
+import { ReviewMovieComponent } from './components/review-movie/review-movie.component';
 
 const routes: Routes = [
   {path:"", redirectTo:"navbar", pathMatch:"full"},
   {path:"navbar", component:NavbarComponent},
+  {path:"review-movie", component:ReviewMovieComponent}
+
+  
 
 ];
 
--- MovieApp/src/app/app.module.ts
+++ MovieApp/src/app/app.module.ts
@@ -1,18 +1,26 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import{RouterModule, Routes} from '@angular/router';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './components/login/login.component';
 import { RegisterComponent } from './components/register/register.component';
 import { NavbarComponent } from './navbar/navbar.component';
+import { ReviewMovieComponent } from './components/review-movie/review-movie.component';
 
+const routes: Routes=[
+  {path: 'review-movie',
+component: ReviewMovieComponent}
+];
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
     RegisterComponent,
-    NavbarComponent
+    NavbarComponent,
+    ReviewMovieComponent,
+   
   ],
   imports: [
     BrowserModule,

3cd41ff028c2a5c742acc1940c5e023dfb487966
DA M1 41 <dam141@DAs-MacBook-Air.local>
2024-04-25 16:24:06 +0200

    Movie cards

--- MovieApp/src/app/components/dashboard/dashboard.component.ts
+++ MovieApp/src/app/components/dashboard/dashboard.component.ts
@@ -7,4 +7,16 @@ import { Component } from '@angular/core';
 })
 export class DashboardComponent {
 
+
+  latestMovies = [
+    { title: 'Movie 1', year: 2022, director: 'Director 1' },
+    { title: 'Movie 2', year: 2023, director: 'Director 2' },
+    { title: 'Movie 3', year: 2024, director: 'Director 3' }
+  ];
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
 }

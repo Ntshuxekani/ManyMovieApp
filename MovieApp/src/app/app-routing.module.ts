1322d7848b74ee653fd9d8bef5eae45c3fdb33fb
Sifundo-B <sifundo.shenge12@gmail.com>
2024-04-26 11:28:31 +0200

    adding routing to login

--- MovieApp/src/app/app-routing.module.ts
+++ MovieApp/src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './components/login/login.component';
+import { RegisterComponent } from './components/register/register.component';
 
 const routes: Routes = [
-  {path:"", redirectTo:"login", pathMatch:"full"},
+  {path:"", redirectTo:"register", pathMatch:"full"},
+  {path:"register",component:RegisterComponent},
   {path:"login", component:LoginComponent}
 ];
 @NgModule({

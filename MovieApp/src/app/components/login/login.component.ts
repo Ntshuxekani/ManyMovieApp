8c79164ca802f97bd7bd62a92d44b29c9add6c06
sihle1996 <mofokengsihle63@gmail.com>
2024-05-24 08:04:39 +0200

    ..

--- MovieApp/src/app/components/login/login.component.ts
+++ MovieApp/src/app/components/login/login.component.ts
@@ -27,25 +27,31 @@ export class LoginComponent implements OnInit {
     });
   }
   onLogin() {
-    this.authService.signin(this.email, this.password);
-  }
-  login(): void {
-    this.http.get<any>("http://localhost:8080/api/login")
-      .subscribe(res => {
-        const user = res.find((a: any) => {
-          return a.email === this.loginForm.value.email && a.password === this.loginForm.value.password
-        });
-        if (user) {
-          alert('Login Successful');
-          this.authService.signin(this.loginForm.value.email,this.loginForm.value.password); // Update user's authentication state
-         
-          this.loginForm.reset();
-          this.router.navigate(["home"]); // Navigate to the desired page after login
-        } else {
-          alert("User not found");
-        }
-      }, err => {
-        alert("Something went wrong");
-      });
+    if(this.loginForm.valid){
+      const email = this.loginForm.value.email;
+      const password = this.loginForm.value.password;
+      this.authService.signin(this.email, this.password);
+
     }
+   
+  }
+  // login(): void {
+  //   this.http.get<any>("http://localhost:8080/api/login")
+  //     .subscribe(res => {
+  //       const user = res.find((a: any) => {
+  //         return a.email === this.loginForm.value.email && a.password === this.loginForm.value.password
+  //       });
+  //       if (user) {
+  //         alert('Login Successful');
+  //         this.authService.signin(this.loginForm.value.email,this.loginForm.value.password); // Update user's authentication state
+         
+  //         this.loginForm.reset();
+  //         this.router.navigate(["home"]); // Navigate to the desired page after login
+  //       } else {
+  //         alert("User not found");
+  //       }
+  //     }, err => {
+  //       alert("Something went wrong");
+  //     });
+  //   }
   }
